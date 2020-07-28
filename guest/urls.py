3abf66e2b708441c5c8815c886b03f66e5fbd32a
HYS0988 <930371720@qq.com>
2020-07-28 17:01:49 +0800

    安装Django-bootstrap3之后做的更改-81

--- guest/urls.py
+++ guest/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'^account/login/$', views.index),
     url(r'^login_action/', views.login_action),
     url(r'^event_manage/', views.event_manage),
+    url(r'^guest_manage/$', views.guest_manage),
     url(r'^search_name/$', views.search_name),
 ]
--- sign/views.py
+++ sign/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event
+from sign.models import Guest
 
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
 
 def login_action(request):
     if request.method == 'POST':
@@ -22,6 +24,7 @@ def login_action(request):
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
 
+
 @login_required
 def event_manage(request):
     Event_list = Event.objects.all()
@@ -30,10 +33,20 @@ def event_manage(request):
     return render(request, 'event_manage.html', {'user': username,
                                                  'events': Event_list})
 
-login_required
+
+@login_required
 def search_name(request):
     username = request.session.get('user', '')
     search_name = request.GET.get('name', '')
-    event_list = Event.objects.filter(name__contains=search_name)
+    event_list = Guest.objects.filter(name__contains=search_name)
     return render(request, 'event_manage.html', {'user': username,
                                                  'events': event_list})
+
+
+@login_required
+def guest_manage(request):
+    username = request.session.get('user', '')
+    guest_list = Guest.objects.all()
+    # print('event:{}'.format(guest_list))
+    return render(request, 'guest_manage.html', {'user': username,
+                                                 'events': guest_list})

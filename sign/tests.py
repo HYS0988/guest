79ad15a44f41e8c3f198257e68e9b808c4c6b5fa
HYS0988 <930371720@qq.com>
2020-08-05 22:07:13 +0800

    增加测试功能-101

--- sign/tests.py
+++ sign/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+from sign.models import Event
+from sign.models import Guest
 # Create your tests here.
+
+
+class ModuleTest(TestCase):
+
+    def setUp(self) -> None:
+        Event.objects.create(id=1, name='oneplus 3 event',
+                            status=True, limit=200,
+                            address='shenzhen',
+                            start_time='2016-08-31 02:18:22')
+        Guest.objects.create(id=1, event_id=1,
+                             realname='alen',
+                             phone='13711001101',
+                             email='alen@mail.com',
+                             sign=False)
+
+    def test_event_modles(self):
+        result = Event.objects.get(name='oneplus 3 event')
+        self.assertEqual(result.address, 'shenzhen')
+        self.assertTrue(result.status)
+
+    def test_guest_modles(self):
+        result = Guest.objects.get(phone='13711001101')
+        self.assertEqual(result.realname, 'alen')
+        self.assertFalse(result.sign)
